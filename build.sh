c1d4c3d44e58ee775c4a0fbc134d095f6aaa85d3
flapperz <flap.atyz@gmail.com>
2024-09-25 22:33:17 +0700

    chore: add build command

---
+++ build.sh
@@ -0,0 +1,15 @@
+# echo '{ "main": "hello.js", "output": "sea-prep.blob" }' > sea-config.json
+
+BUILD_DIR='build'
+CLI_NAME='mycli'
+CLI_PATH=${BUILD_DIR}/${CLI_NAME}
+
+node --experimental-sea-config sea-config.json
+cp $(command -v node) $CLI_PATH
+codesign --remove-signature $CLI_PATH
+
+npx postject $CLI_PATH NODE_SEA_BLOB $BUILD_DIR/sea-prep.blob \
+    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
+    --macho-segment-name NODE_SEA b2
+
+codesign --sign - $CLI_PATH
