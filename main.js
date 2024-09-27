3180a84553b6e2dd66af4470575c4e67acfbba13
flapperz <flap.atyz@gmail.com>
2024-09-27 14:18:02 +0700

    feat: read json configuration

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const util = require("node:util");
+const fs = require("node:fs");
 
 const argsConfig = {
-  options: {},
-  allowPositionals: true,
+  options: {
+    preset: {
+      type: "string",
+      short: "p",
+    },
+  },
+  allowPositionals: false,
 };
 
 const { values, positionals } = util.parseArgs(argsConfig);
 console.log("Parsed argument:", values, positionals);
+
+// const config = fs.readFile("~/.config/misc/ybjs.rc");
+const configFileName = "./res/ybjs.json";
+const config = JSON.parse(fs.readFileSync(configFileName, "utf8"));
+console.log("Config:", config);
+
+const { set: setPreset } = values;
+
+console.log(setPreset);
+// validate arguments
+
+if ()
